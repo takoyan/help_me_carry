4c1cd97a3ac629d8293724af8f7bd1e6d430db2a
takoyan <is0393he@ed.ritsumei.ac.jp>
2019-06-16 14:21:21 +0900

    調整

--- scripts/help.py
+++ scripts/help.py
@@ -130,13 +130,13 @@ def help():
             
             
     rospy.init_node('help', anonymous=True)
-    start_resume=rospy.Publisher('txt_start', Bool, queue_size=10)
-    yes_no=rospy.Publisher('yes_no_start', Bool, queue_size=10)
-    send_place=rospy.Publisher('help_me_carry/send_place', String, queue_size=10)
-    speak=rospy.Publisher('help_me_nlp_second_half/speak_sentence', String, queue_size=10)
+    start_resume=rospy.Publisher('help_me_nlp_second_half_recognition', Bool, queue_size=10)#音声認識開始
+    yes_no=rospy.Publisher('yes_no_start', Bool, queue_size=10)#yes_no取得開始
+    send_place=rospy.Publisher('help_me_carry/send_place', String, queue_size=10)#場所情報送信
+    speak=rospy.Publisher('help_me_nlp_second_half/speak_sentence', String, queue_size=10)#発話開始
     rospy.Subscriber('help_ctrl', String, start_speech)#起動用
-    rospy.Subscriber('recognition_result', String, get_yesno)#yes_no
-    rospy.Subscriber('recognition_txt', String, get_txt)#txt
+    rospy.Subscriber('help_me_nlp_second_half/recognition_result', String, get_yesno)#yes_no
+    rospy.Subscriber('help_me_nlp_second_half/recognition_result', String, get_txt)#音声認識結果取得
     rospy.Subscriber('help_me_nlp_second_half/finish_speaking', Bool, finish_speaking)
     rospy.spin()
 
